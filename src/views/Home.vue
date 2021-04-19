ace64c838bdf88441344c6b7af6ac2dc8a5ae816
Chutipa Jatujinda <chutipa.cchutp@mail.kmutt.ac.th>
2021-04-19 17:53:59 +0700

    [ADD] 62130500122 add and edit form under banner picture

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <div :style="image">
+  <div >
     <body class="h-full m-0">
       <MenuBar></MenuBar> 
       <img src="../assets/mountain.png" class="w-screen ml-auto mr-auto">
-      <Form @submitted-favoriteList="addNewFavorite"></Form>
+      <Form @close="changeAddFavoriteClick" @submitted-favoriteList="addNewFavoriteList"></Form>
       <Gallery></Gallery> 
     </body>
   </div>
@@ -15,17 +15,47 @@
 //import MenuBar from '../components/MenuBar.vue'
 import Form from '@/components/Form.vue'
 import MenuBar from '@/components/MenuBar.vue'
-import Gallery from '../components/Gallery.vue'
+// import Gallery from '@/components/Gallery.vue'
 
 export default {
   name: 'Home',
   components:{
-   MenuBar,Form,Gallery
+   MenuBar,Form
   },
-  data(){
+  data() {
     return {
-      image:{ backgroundImage:"url(../asset.mountain.png)"}
+      image: {backgroundImage: "url(../assets/mountain.png)"},
+      url: "http://localhost:3000/favoriteList",
+			favoriteList: [],
+			editClick: false,
+    };
+  },
+  methods: {
+    changeAddFavoriteClick(obj){
+      this.$emit("close-form",obj);
+    },
+    async fetchFavorite(){
+      const res = await fetch (this.url);
+      const data = await res.json();
+      return data;
+    },
+    async addNewFavoriteList(addNewFavoriteList){
+      const res = await fetch(this.url,{
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          country: addNewFavoriteList.country,
+          place: addNewFavoriteList.place
+        })
+      });
+      const data = await res.json();
+      this.favoriteList = [...this.favoriteList, data];
     }
-  }
-}
+  },
+  async created() {
+    this.favoriteList = await this.fetchFavorite();
+  },
+};
 </script>
